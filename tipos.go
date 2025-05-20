bac3230fdfbe2b3d2954db5e049feb7c8fa34834
Thalita <thalitascruz@icloud.com>
2025-05-20 14:44:40 -0300

    Adiciona tipos.go em Go

---
+++ tipos.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// bool (true/false)
+	fmt.Printf("Type: %T - Value %v\n", true, true)
+
+	//string - sequência de bytes
+	fmt.Printf("Type: %T - Value %v\n", "Tata", "Tata")
+	fmt.Printf("Type: %T - Value %v\n", "1", "1")
+
+	// int
+	fmt.Printf("Type: %T - Value %v\n", 1, 1)
+
+	// float (float64/float32) - decimal
+	fmt.Printf("Type: %T - Value %v\n", 1.233, 1.233)
+}
