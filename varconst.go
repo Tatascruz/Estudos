769066e76415cbf34f9d7fec58f1c2dd320db23c
Thalita <thalitascruz@icloud.com>
2025-05-22 21:08:22 -0300

    Adiciona tipos de Variáveis e Constantes em Go

---
+++ varconst.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variáveis
+	// var + nome da variavel + tipo
+	var nome string
+	nome = "bento"
+	fmt.Println(nome)
+
+	nome = "Tata"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 4
+	fmt.Println(idade)
+
+	var a = "Tata"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	f := "apple"
+	fmt.Println(f)
+
+	numero := "Tata"
+	fmt.Println(numero)
+
+	// Constantes
+	const idadeBento = 4
+	fmt.Println(idadeBento)
+
+}
