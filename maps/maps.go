d80b3a14a0b50c17d807d8cac22ebf68825d8c11
Thalita <thalitascruz@icloud.com>
2025-06-09 07:14:47 -0300

    Adiciona Maps na linguagem Go

--- maps/maps.go
+++ maps/maps.go
@@ -10,4 +10,15 @@ func MostrarMaps() {
 	fmt.Printf("%v\n", idade)
 	fmt.Printf("%v\n", idade["tata"])
 	fmt.Printf("%v\n", idade["bento"])
+
+	anoNasc := map[string]int{
+		"tata":  1985,
+		"bento": 2008,
+	}
+	fmt.Println(anoNasc)
+	fmt.Println(anoNasc["tata"])
+	fmt.Println(anoNasc["bento"])
+	anoNasc["golangDoZero"] = 2024
+	fmt.Println(anoNasc)
+
 }
