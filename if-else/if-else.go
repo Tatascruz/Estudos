9c9090c441072c45e7dece58000070669dd3564f
Thalita <thalitascruz@icloud.com>
2025-06-16 08:16:49 -0300

    Adiciona if-else na linguagem Go

--- if-else/if-else.go
+++ if-else/if-else.go
@@ -7,10 +7,39 @@ import "fmt"
 
 func MostraIfElse() {
 
-	valor := 1
+	//numero := 3
 	//if (condição) { <ação> }
-	if valor == 1 { // true
-		fmt.Println("Valor é igual a 1")
+	//if numero == 2 { // true
+	//	fmt.Println("Valor é igual a 1")
+	//} else {
+	//	fmt.Println("valor não é igual a 2")
+	//}
 
-	}
+	//if numero == 1 {
+	//fmt.Println("Valor é igual a 1")
+	//} else if numero == 2 {
+	//fmt.Println("Valor é igual a 2")
+	//} else {
+	//fmt.Println("Valor é diferente de 1 e 2")
+	//}
+
+	//numero := 7
+	//if numero%2 == 0 {
+	//	fmt.Printf("%d é par", numero)
+	//} else {
+	//	fmt.Printf("%d é impar", numero)
+	//}
+
+	fmt.Println(1 + 1)
+	fmt.Println(2 - 1)
+	fmt.Println(10 / 2)
+	fmt.Println(2 * 2)
+	fmt.Println(10 % 2)
+
+	// Operações
+	// Soma: 1 + 1
+	// Subtração: 2 - 1
+	// Divisão: 10 / 2
+	// Multiplicação: 2 * 2
+	// Resto da divisão po x: 7%2 (resto da divisão por 2)
 }
