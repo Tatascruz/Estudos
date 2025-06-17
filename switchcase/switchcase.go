fbaa58ce0fa085ede3818201e1bef59f1ef68c9e
Thalita <thalitascruz@icloud.com>
2025-06-17 08:04:51 -0300

    Adiciona Switchcase em linguagem Go

--- switchcase/switchcase.go
+++ switchcase/switchcase.go
@@ -6,13 +6,23 @@ import (
 
 func MostarSwitchcase() {
 
-	posicao := 2
-	switch posicao {
-	case 1:
-		fmt.Println("Primeiro lugar")
-	case 2:
-		fmt.Println("Segundo lugar")
-	case 3:
-		fmt.Println("Terceiro lugar")
+	// posicao := 2
+	// switch posicao {
+	// case 1:
+	//	fmt.Println("Primeiro lugar")
+	// case 2:
+	//	fmt.Println("Segundo lugar")
+	// case 3:
+	//	fmt.Println("Terceiro lugar")
+	// }
+
+	nome := "bob"
+	switch nome {
+	case "tata":
+		fmt.Println("É uma pessoa")
+	case "bento":
+		fmt.Println("É um cachorro")
+	case "bob":
+		fmt.Println("É um personagem")
 	}
 }
